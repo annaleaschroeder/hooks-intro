d06ed4e344991064f772ba2b34d8c3d7961afd1a
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-08-12 12:41:45 +0200

    Add buttons and counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
-function App() {
-  return <div></div>
-}
+import { useState } from 'react'
 
 export default App
+
+function App() {
+  const [counterValue, setCounterValue] = useState(0)
+  const isZero = counterValue === 0 // hier wird eine Const für null gesetzt, um später weniger schreiben zu müssen
+  return (
+    <div>
+      <p>{counterValue}</p>
+      <button onClick={countUp}>Increase number</button>
+      <button disabled={isZero} onClick={countDown}>
+        Decrease
+      </button>
+    </div>
+  )
+  function countUp() {
+    setCounterValue(counterValue + 1)
+  }
+  function countDown() {
+    !isZero && setCounterValue(counterValue - 1)
+    //const isNotZero = !isZero könnte man theoretisch drüber setzen. Alternative: isZero || setCounterValue(counterValue - 1)
+  }
+}
